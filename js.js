e1b9ff0d81e726e264407103ac066a848205f9c5
Deepak9712 <131691883+Deepak9712@users.noreply.github.com>
2024-03-30 19:47:23 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+document.getElementById("btn1").addEventListener("click",()=>{
+        
+    document.getElementById("btn1").style.backgroundColor="black";
+    document.getElementById("btn1").style.color="white";
+    document.getElementById("btn2").style.backgroundColor="white"
+    document.getElementById("btn2").style.color="black"
+})
+document.getElementById("btn2").addEventListener("click",()=>{
+    
+    document.getElementById("btn2").style.backgroundColor="black";
+    document.getElementById("btn2").style.color="white";
+    document.getElementById("btn1").style.backgroundColor="white"
+    document.getElementById("btn1").style.color="black"
+})
